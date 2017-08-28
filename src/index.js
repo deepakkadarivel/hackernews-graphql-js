bd53487796e1693d260d71b23c9f362ea03d465b
Deepakkv <deepakkv@in-deepakkv.local>
2017-08-28 17:58:31 +0530

    Authentication

--- src/index.js
+++ src/index.js
@@ -1,23 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {graphqlExpress, graphiqlExpress} = require('apollo-server-express');
+const {authenticate} = require('./authentication');
 
 const schema = require('./schema');
 
 const connectMongo = require('./mongo-connector');
 const start = async () => {
     const mongo = await connectMongo();
-
     var app = express();
-    app.use('/graphql', bodyParser.json(), graphqlExpress({
-        context: {mongo},
-        schema
-    }));
+
+    const buildOptions = async (req, res) => {
+        const user = await authenticate(req, mongo.Users);
+        return {
+            context: {mongo, user},
+            schema,
+        };
+    };
+    app.use('/graphql', bodyParser.json(), graphqlExpress(buildOptions));
+
     app.use('/graphiql', graphiqlExpress({
         endpointURL: '/graphql',
+        passHeader: `'Authorization': 'bearer token-foo@bar.com'`,
     }));
 
-    const PORT = 3000;
+    const PORT = 3030;
     app.listen(PORT, () => {
         console.log(`Hacker news Graphql server running at port ${PORT}`);
     });
